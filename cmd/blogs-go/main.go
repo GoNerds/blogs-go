632a595ec03c11bb1e751fdf7d11b70c00a97571
Robert Stevens <robert.stevens86091@gmail.com>
2024-07-21 01:17:50 -0400

    HTTP server created, Router added, Database connected

--- cmd/blogs-go/main.go
+++ cmd/blogs-go/main.go
@@ -1,1 +1,22 @@
+package main
 
+import (
+	"log"
+	"net/http"
+
+	"blogs-go/db"
+	"blogs-go/routes"
+)
+
+func main() {
+	//Initialize the database
+
+	db.Connect("mongodb://localhost:27017", "blogs-go")
+	defer db.Close()
+
+	r := routes.InitializeRoutes(db.GetDatabase())
+
+	http.Handle("/", r)
+	log.Println("Server started on port 5000")
+	log.Fatal(http.ListenAndServe(":5000", nil)
+}
--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,1 +1,28 @@
+package routes
 
+import (
+	"blogs-go/handlers"
+	"blogs-go/repository"
+
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func NewRouter(userRepo *repository.UserRepository) *mux.Router {
+	userHandler := handlers.NewUserHandler(userRepo)
+	
+	r := mux.NewRouter()
+
+	r.HandleFunc("/users", userHandler.CreateUser).Methods("POST")
+	r.HandleFunc("/users", userHandler.GetUsers).Methods("GET")
+	r.HandleFunc("/users/{id}", userHandler.GetUserByID).Methods("GET")
+	r.HandleFunc("/users/{id}", userHandler.UpdateUser).Methods("PUT")
+	r.HandleFunc("/users/{id}", userHandler.DeleteUser).Methods("DELETE")
+
+	return r
+}
+
+func InitializeRoutes(db *mongo.Database) *mux.Router {
+	userRepo := repository.NewUserRepository(db)
+	return NewRouter(userRepo)
+}
